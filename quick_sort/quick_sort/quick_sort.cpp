a503fc1fa2f5e14c9741492ba606cec21ea09b3c
Lolomyn <61377364+Lolomyn@users.noreply.github.com>
2020-03-26 11:47:35 +0300

    Добавление обработки ввода

--- quick_sort/quick_sort/quick_sort.cpp
+++ quick_sort/quick_sort/quick_sort.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream> 
+#include <iostream> 
 #include "quick_sort.h" 
 
 using namespace std;
@@ -18,10 +18,18 @@ int main()
 {
 	int n; // размер массива
 	cout << "Array Size: ";
-	cin >> n;
-	cout << endl;
-	int* arr = new int[n]; // массив размером n
+	//--- Обработка ввода ---\\\ 
+	while (!(cin >> n) || n < 0)
+	{
+		cout << "Uncorrect size, try again \n";
+		cout << "Array Size: ";
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	}
+	cout << endl; 
+	//--- ---\\\
 
+	int* arr = new int[n]; // массив размером n
 	//--- Ввод n элементов массив поочереди ---\\ 
 	for (int i = 0; i < n; i++)
 	{
@@ -31,9 +39,8 @@ int main()
 	}
 	//--- ---\\ 
 
-	print(arr, n); // вывод на экран введенной 
-        // последовательности до сортировки
-	quickSort(arr, 0, n-1); // функция сортировки
+	print(arr, n); // вывод на экран введенной последовательности
+	quickSort(arr, 0, n - 1); // функция сортировки
 	print(arr, n); // вывод на экран результата сортировки
 	system("pause");
 }
