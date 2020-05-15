9732fc47b05cd3853164711b3b2a8992f1a14674
Куликова Елена Александровна <61598989+Rayfell@users.noreply.github.com>
2020-05-15 14:55:38 +0300

    Код курсовой работы с windows forms

---
+++ KulikovaEA/CourseWork/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ChompWF
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Main main = new Main(comboBox1.SelectedIndex + 1, comboBox2.SelectedIndex + 1, radioButton1.Checked);
+            Hide();
+            if (main.ShowDialog() == DialogResult.Cancel)
+                Close();
+        }
+    }
+}
